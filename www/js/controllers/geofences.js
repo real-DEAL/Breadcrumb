e4e9c37f868832f1b12cf0878296a96a8e7a6ebd
defields923 <defields923@gmail.com>
2017-03-13 11:19:44 -0500

    (refactor) moved files into working order

---
+++ www/js/controllers/geofences.js
@@ -0,0 +1,79 @@
+angular.module("ionic-geofence").controller("GeofencesCtrl", function (
+    $scope,
+    $ionicActionSheet,
+    $timeout,
+    $log,
+    $state,
+    Geolocation,
+    Geofence,
+    $ionicLoading
+) {
+    $ionicLoading.show({
+        template: "Getting geofences from device...",
+        duration: 5000
+    });
+
+    $scope.geofences = [];
+
+    Geofence.getAll().then(function (geofences) {
+        $ionicLoading.hide();
+        $scope.geofences = geofences;
+    }, function (reason) {
+        $ionicLoading.hide();
+        $log.error("An Error has occured", reason);
+    });
+
+    $scope.createNew = function () {
+        $log.log("Obtaining current location...");
+        $ionicLoading.show({
+            template: "Obtaining current location...",
+            hideOnStateChange: true
+        });
+
+        Geolocation.getCurrentPosition()
+            .then(function (position) {
+                $log.log('sup');
+                $log.info("Current position found", position);
+
+                $state.go("app.geofence-new", {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            }, function (reason) {
+                $log.error("Cannot obtain current location", reason);
+                $ionicLoading.show({
+                    template: "Cannot obtain current location",
+                    duration: 1500
+                });
+            });
+    };
+
+    $scope.editGeofence = function (geofence) {
+        $state.go("geofence-edit", {
+            geofenceId: geofence.id
+        });
+    };
+
+    $scope.removeGeofence = function (geofence) {
+        Geofence.remove(geofence);
+    };
+
+    $scope.more = function () {
+        // Show the action sheet
+        $ionicActionSheet.show({
+            titleText: "More options",
+            buttons: [
+                { text: "<i class='icon ion-checkmark-circled'></i> Test application" }
+            ],
+            destructiveText: "<i class='icon ion-trash-b'></i> Delete all geofences",
+            cancelText: "<i class='icon ion-android-cancel'></i> Cancel",
+            destructiveButtonClicked: function () {
+                Geofence.removeAll();
+                return true;
+            },
+            buttonClicked: function() {
+                window.location.href = "cdvtests/index.html";
+            }
+        });
+    };
+});
