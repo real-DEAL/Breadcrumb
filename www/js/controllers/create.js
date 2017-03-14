5a9b6ac0e0495ae34448bec5ca3eb34d23945d53
phrush <w.phrush@gmail.com>
2017-03-14 14:30:42 -0500

    (feature) cards swipe left and right with buttons now

--- www/js/controllers/create.js
+++ www/js/controllers/create.js
@@ -2,7 +2,13 @@
 /* global TransitionType */
 
 angular.module('ionic-geofence')
-.controller('createTrailCtrl', function ($scope) {
+.controller('createTrailCtrl', function ($scope, Trail) {
+  $scope.style = function () {
+    return {
+      left: '2.5%',
+    };
+  };
+
   $scope.trail = {
     name: '',
     description: '',
@@ -10,33 +16,54 @@ angular.module('ionic-geofence')
     money: false,
     steps: 0,
     length: '',
+    left: 2.5,
+    style: $scope.style(),
   };
 
-  $scope.step = {
-    text: '',
-    location: '',
-    media: '',
+  $scope.step = function () {
+    return {
+      text: '',
+      location: '',
+      media: '',
+      left: 2.5,
+      style: $scope.style(),
+    };
   };
 
   $scope.steps = [
   ];
 
   $scope.add = function () {
-    const step = {
-      text: '',
-      location: '',
-      media: '',
-    };
+    $scope.move(-100);
+    $scope.trail.steps += 1;
+    const step = $scope.step();
     $scope.steps.push(step);
   };
 
   $scope.cardSwipedLeft = function () {
-    console.log('LEFT SWIPE');
+    // console.log('LEFT SWIPE');
     // $scope.addCard();
   };
 
   $scope.cardSwipedRight = function () {
-    console.log('RIGHT SWIPE');
+    // console.log('RIGHT SWIPE');
     // $scope.addCard();
+  };
+
+  $scope.move = function (num) {
+    let move = `${$scope.trail.left += num}%`;
+    let style = {
+      left: move,
+      'transition-duration': '500ms',
+    };
+    $scope.trail.style = style;
+    $scope.steps.forEach(function (step) {
+      move = `${step.left += num}%`;
+      style = {
+        left: move,
+        'transition-duration': '500ms',
+      };
+      step.style = style;
+    });
   };
 });
---
+++ www/js/services/create.js
@@ -0,0 +1,17 @@
+/* global UUIDjs */
+/* global TransitionType */
+/* global localStorage */
+/* eslint no-underscore-dangle: ["error", { "allow": ["_geofences", "_geofencesPromise"] }] */
+angular.module('ionic-geofence').factory('Trail', function (
+  $rootScope,
+  $window,
+  $q,
+  $log,
+  $ionicLoading
+) {
+  const trailFactory = {
+
+  };
+
+  return trailFactory;
+});
