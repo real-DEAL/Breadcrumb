b2350ca315be3b82f6abfd82be5052bcda112629
phrush <w.phrush@gmail.com>
2017-03-14 17:42:16 -0500

    (feature) dashboard page list items toggle to reveal extra info (feature) rating and difficulty displayed as icons

--- www/js/controllers/list.js
+++ www/js/controllers/list.js
@@ -3,17 +3,66 @@
 
 angular.module('ionic-geofence')
 .controller('ListCtrl', function ($scope) {
-  $scope.exampleTrail = {
+  const closeStyle = {
+    height: '45px',
+    'transition-duration': '250ms',
+  };
+
+  const trailmaker = function () {
+    return Object.create($scope.trail);
+  };
+
+  $scope.trail = {
     name: 'My first trail',
-    rating: 5,
+    rating: 3,
     difficulty: 3,
     length: 25,
     progress: '50%',
+    style: { height: '45px' },
+  };
+
+  $scope.stars = function () {
+    return _.range($scope.trail.rating);
+  };
+
+  $scope.nostars = function () {
+    return _.range(5 - $scope.trail.rating);
+  };
+
+  $scope.difficulty = function () {
+    return _.range($scope.trail.difficulty);
+  };
+
+  $scope.toggle = function (index) {
+    return $scope.trails[index].style.height === '45px' ?
+    $scope.open(index) :
+    $scope.close(index);
+  };
+
+  $scope.open = function (index) {
+    $scope.trails[index].style = {
+      height: '320px',
+      'transition-duration': '250ms',
+    };
+    $scope.trails.forEach(function (trail, place) {
+      if (place !== index) {
+        $scope.close(place);
+      }
+    });
+  };
+
+  $scope.close = function (index) {
+    $scope.trails[index].style = closeStyle;
   };
 
   $scope.trails = [
-    $scope.exampleTrail,
-    $scope.exampleTrail,
-    $scope.exampleTrail,
+    trailmaker(),
+    trailmaker(),
+    trailmaker(),
+    trailmaker(),
+    trailmaker(),
+    trailmaker(),
+    trailmaker(),
+    trailmaker(),
   ];
 });
