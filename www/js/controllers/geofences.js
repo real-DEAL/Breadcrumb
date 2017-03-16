2ed85f997f7093df4436f198ff3ac7c2f8356582
Alice Lai <aplai168@gmail.com>
2017-03-15 20:07:10 -0500

    (cleanup)

--- www/js/controllers/geofences.js
+++ www/js/controllers/geofences.js
@@ -11,9 +11,9 @@ angular.module('breadcrumb').controller('GeofencesCtrl', function (
   $ionicLoading
 ) {
   $ionicLoading.show({ template: 'Getting geofences from device...', duration: 5000 });
-  // var directionsDisplay = new google.maps.DirectionsRenderer();
-  const directionsService = new google.maps.DirectionsService();
+
   $scope.geofences = [];
+
   Geofence.getAll().then((geofences) => {
     $ionicLoading.hide();
     $scope.geofences = geofences;
@@ -66,55 +66,6 @@ angular.module('breadcrumb').controller('GeofencesCtrl', function (
       buttonClicked: () => {
         window.location.href = 'cdvtests/index.html';
       },
-    });
-  };
-
-  $scope.directions = {
-    origin: '35 Madewood Dr, Marrero, LA 70072',
-    waypoints: ['5727 Brighton Pl, New Orleans, LA 70131'],
-    destination: '748 Camp St, New Orleans, LA 70130',
-    showList: false,
-  };
-  const computeTotalDistance = (response) => {
-    let total = 0;
-    const myRoute = response.routes[0];
-    myRoute.legs.forEach((leg) => { total += leg.distance.value; });
-    total /= 1000;
-    console.warn(total, 'km total');
-  };
-  const totalMiles = (response) => {
-    const myRoute = response.routes[0].legs[0].distance.text;
-      console.warn(myRoute);
-  };
-  const computeTotalDuration = (response) => {
-    // let total = 0;
-    const myRoute = response.routes[0].legs[0].duration.text;
-    // myRoute.legs.forEach((leg) => { total += leg.duration.value; });
-    console.warn(myRoute, 'mins total');
-  };
-  const arrayPathAddOn = (response) => {
-    let res = '';
-    const myRoute = response.routes[0];
-    res = myRoute.overview_polyline;
-    return res;
-  };
-  const url = 'http://maps.googleapis.com/maps/api/staticmap?size=400x400&path=enc:';
-  $scope.addPath = () => {
-    const request = {
-      origin: $scope.directions.origin,
-      destination: $scope.directions.destination,
-      travelMode: google.maps.DirectionsTravelMode.DRIVING,
-    };
-    directionsService.route(request, (response, status) => {
-      if (status === google.maps.DirectionsStatus.OK) {
-        computeTotalDistance(response);
-        computeTotalDuration(response);
-        totalMiles(response);
-        arrayPathAddOn(response);
-        console.warn(`${url}${arrayPathAddOn(response)}`);
-      } else {
-        console.warn('Google route unsuccessful!');
-      }
     });
   };
 });
