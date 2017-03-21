444be015bbdb13ac8e02f888835b8e7c4b84cf9b
defields923 <defields923@gmail.com>
2017-03-21 10:57:34 -0500

    (cleanup) remove unnecessary

--- www/js/controllers/camera.js
+++ www/js/controllers/camera.js
@@ -1,13 +1,6 @@
 angular.module('breadcrumb').controller('CameraCtrl', function (
   $scope
 ) {
-  ezar.initializeVideoOverlay(() => {
-    ezar.getBackCamera().start();
-    // document.body.style.backgroundColor = 'transparent';
-  }, (error) => {
-    console.error('ezar initialization failed', error);
-  });
-
   $scope.takePicture = () => {
     function onSuccess(imageData) {
       const image = document.getElementById('myImage');
