8786944132f2495de00fd4c3163362907b920bc5
phrush <w.phrush@gmail.com>
2017-03-22 13:05:53 -0500

    (feature) refresh button on dashboard (feature) images hosted on cloudinary

--- www/js/controllers/trail.js
+++ www/js/controllers/trail.js
@@ -33,7 +33,7 @@ angular.module('breadcrumb')
     //   description: 'A famous genera\'s statue, surrounded by controversy!',
     //   name: 'Lee\'s Circle',
     // media_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    //   image: '../img/breadfalls.gif',
+    //   image: 'http://res.cloudinary.com/realdeal/image/upload/v1490205277/breadfalls_cnvvbe.gif',
     //   video: 'https://www.youtube.com/embed/ZuA6bPvHvwE',
     //   ar: null,
     //   audio: null,
