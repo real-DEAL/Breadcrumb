389990deefb3bb1e867ce8facda7a52855d281df
Alice Lai <aplai168@gmail.com>
2017-03-25 17:15:30 -0500

    (feature) map marker updates location name

--- www/js/controllers/create.js
+++ www/js/controllers/create.js
@@ -332,6 +332,7 @@ angular.module('breadcrumb')
         },
       },
 
+
       markers: {
         marker: {
           lat: 29.9511,
@@ -407,6 +408,7 @@ angular.module('breadcrumb')
 
   });
 
+
   $scope.updateMap = () => {
     $scope.center = {
       lat: $scope.location.lat,
@@ -474,7 +476,7 @@ angular.module('breadcrumb')
     $scope.center.lng = center.lng;
     $scope.location.lat = center.lat;
     $scope.location.lng = center.lng;
-    $scope.updateMap();
+    // $scope.updateMap();
     $scope.markers = {
       marker: {
         lat: $scope.center.lat,
@@ -490,27 +492,107 @@ angular.module('breadcrumb')
     console.warn(args, 'args');
     // now make this new args populate the input value !!!!
     const map = args.leafletEvent.target;
+
     // const center = (map.getCenter());
     const center = map.getLatLng();
+    // console.log(map.panTo(center), 'going to pan to the center ')
+
     // change the center based on where you placed the marker
     $scope.center.lat = center.lat;
     $scope.center.lng = center.lng;
     // change the location which speaks to the input box in view
     $scope.location.lat = center.lat;
     $scope.location.lng = center.lng;
-    $scope.updateMap();
-    $scope.markers = {
-      marker: {
-        lat: $scope.center.lat,
-        lng: $scope.center.lng,
-      },
-    };
-    console.warn($scope.markers.marker.lat, 'change in marker');
-    console.warn($scope.markers.marker.lng);
+    // $scope.updateMap();
+
+    console.warn($scope.location, 'change in marker');
+
+    // $scope.$watch('location', () => {
+    //   $scope.markers =
+    // }, true);
+    // $timeout(() => {
+    //   // angular.extend($scope, {
+    //   //   markers: {
+    //   //     marker: {
+    //   //       lat: center.lat,
+    //   //       lng: center.lat,
+    //   //       draggable: true,
+    //   //     },
+    //   //   },
+    //   // });
+      // $scope.$apply(() => {
+      //   $scope.markers.marker.lat = center.lat;
+      //   $scope.markers.marker.lng = center.lng;
+      // });
+    //
+    // });
+    // $scope.$watchGroup((['center.lat', 'center.lng']), () => {
+    //    $scope.markers.marker.lat = center.lat;
+    //    $scope.markers.marker.lng = center.lng;
+    // });
+    // $scope.$apply();
+    // $scope.$apply(() => {
+    //   $scope.markers.marker.lat = center.lat;
+    //   $scope.markers.marker.lng = center.lng;
+    // });
+
+    // $scope.safeApply(function() {
+    //   // im extending the marker to the new marker location when it moves
+    //   angular.extend($scope, {
+    //     markers: {
+    //       marker: {
+    //         lat: center.lat,
+    //         lng: center.lat,
+    //         draggable: true,
+    //       },
+    //     },
+    //   });
+    // });
+    // console.warn($scope.markers, 'marker info')
+
+    // TODO: HOW TO MAKE THE REVERSE GEOCODE UPDATE WITH THE NEW MARKERS coords
+    // _.defer(() => {
+
+    //   $scope.$apply();
+    // });
+
+    // $scope.$watch('location', () => {
+    //   $scope.markers = {
+    //     marker: {
+    //       lat: $scope.center.lat,
+    //       lng: $scope.center.lng,
+    //     },
+    //   };
+    // });
+    //
+    // $scope.$evalAsync(() => {
+    //   angular.extend($scope, {
+    //     markers: {
+    //       marker: {
+    //         lat: center.lat,
+    //         lng: center.lat,
+    //         draggable: true,
+    //       },
+    //     },
+    //   });
+    // });
+    console.warn($scope.markers.marker.lng, '$evalAsync');
 
     // when the marker changes, make sure that it populates the input box
 
   });
+
+
+
+  // $scope.markers = [];
+  // $scope.$on('leafletDirectiveMap.click', (event, args) => {
+  //   const leafEvent = args.leafletEvent;
+  //   $scope.markers.push({
+  //     lat: leafEvent.latlng.lat,
+  //     lng: leafEvent.latlng.lng,
+  //     draggable: true,
+  //   });
+  // });
 
   $scope.tiles = {
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
