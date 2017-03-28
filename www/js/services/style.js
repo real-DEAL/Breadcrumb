a198ca6b3083b3e9da7415ef0174141fe05b93a7
phrush <w.phrush@gmail.com>
2017-03-28 10:13:22 -0500

    (cleanup) fix minor background image details

--- www/js/services/style.js
+++ www/js/services/style.js
@@ -7,18 +7,36 @@ angular.module('breadcrumb').factory('Style', function () {
         'background-image': 'linear-gradient(#3a7ec7, #c8f0ef)',
       },
       background: '../img/big-mountains.png',
+      static: {
+        'background-color': '#3a7ec7',
+        'background-image': 'url("../img/static-mountains.png")',
+        'background-repeat': 'no-repeat',
+        'background-position': 'bottom center',
+      },
     },
     {
       style: {
         'background-image': 'linear-gradient(#c8f0ef, #3a7ec7)',
       },
       background: '../img/big-houses.png',
+      static: {
+        'background-color': '#c8f0ef',
+        'background-image': 'url("../img/static-houses.png")',
+        'background-repeat': 'no-repeat',
+        'background-position': 'bottom center',
+      },
     },
     {
       style: {
         'background-image': 'linear-gradient(#3f3f3f, #3a7ec7)',
       },
       background: '../img/big-city.png',
+      static: {
+        'background-color': '#3D5772',
+        'background-image': 'url("../img/static-city.png")',
+        'background-repeat': 'no-repeat',
+        'background-position': 'bottom center',
+      },
     },
   ];
 
