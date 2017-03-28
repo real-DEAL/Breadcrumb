f8c6214475a38b691f27bd32e90223dce1a6e80e
Eric Periou <eperiou@yahoo.com>
2017-03-28 14:26:01 -0500

    (feature) Update routes on add and update page

--- www/js/services/getAndUpdateUser.js
+++ www/js/services/getAndUpdateUser.js
@@ -9,6 +9,7 @@ angular.module('breadcrumb')
       json: true,
       params: {
         id: userData.id,
+        access_token: store.get('access_token'),
       },
     })
     .then((res) => {
@@ -39,8 +40,8 @@ angular.module('breadcrumb')
   const deleteUserInfo = userData =>
     $http({
       method: 'DELETE',
-      // url: 'http://192.168.99.100:3000/users/${userData.id}',
-      url: `http://54.203.104.113/users/${userData.id}`,
+      // url: `http://192.168.99.100:3000/users/${userData.id}?access_token=${store.get('access_token')}`,
+      url: `http://54.203.104.113/users/${userData.id}?access_token=${store.get('access_token')}`,
       json: true,
     })
     .then(() => {
@@ -53,8 +54,8 @@ angular.module('breadcrumb')
   return (socialID, userInfo, deleteAcct) => {
     $http({
       method: 'GET',
-      // url: 'http://54.203.104.113/users',
-      url: 'http://192.168.99.100:3000/users',
+      // url: `http://54.203.104.113/users?access_token=${store.get('access_token')}`,
+      url: `http://192.168.99.100:3000/users?access_token=${store.get('access_token')}`,
       json: true,
       params: {
         social_login: socialID,
